4fe40a38e60a6ab666482eeee2b0e8a2f8791f56
Nilsen Miotto <daedricmind@outlook.com>
2026-07-02 11:50:57 -0300

    01 - Boas práticas com linters e formatadores

---
+++ 01-ambiente-virtual/main.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+frutas = [
+    "maçã",
+    "banana",
+    "laranja",
+    "abacaxi",
+    "uva",
+    "morango",
+    "kiwi",
+    "pera",
+    "manga",
+    "cereja",
+]
